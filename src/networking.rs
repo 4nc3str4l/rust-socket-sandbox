bf185ec53a411598bf8b4b0368d4a27b304347bb
4nc3str4l <cmuri_10@hotmail.com>
2023-08-07 00:53:05 +0200

    Reading messages from the websocket and sent them to the ui task

--- src/networking.rs
+++ src/networking.rs
@@ -1,22 +1,32 @@
+use std::collections::HashMap;
 use std::time::Duration;
 
 use tokio::sync::mpsc::{Receiver, Sender};
 
 use crate::structs::Message;
 use anyhow::Result;
+use futures_util::{future, pin_mut, StreamExt};
+use tokio_tungstenite::{connect_async};
+use futures_util::stream::SplitSink;
+use tokio_tungstenite::WebSocketStream;
+use tokio_tungstenite::MaybeTlsStream;
+use tokio::net::TcpStream;
 
 
 
 pub async fn network_processor(ui_to_network: &mut Receiver<Message>, network_to_ui: &mut Sender<Message>) {
     
+    let mut connection_map: HashMap<String, SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, tungstenite::Message>> = HashMap::new();
     let net_to_ui = network_to_ui;
     while let Some(message) = ui_to_network.recv().await  {
         println!("Network = {:?}", message);
         match message {
             Message::NewClient { id, ip } => {
-                let res = handle_new_client(net_to_ui.clone(), id.to_owned(), ip.to_owned());
+                let res = handle_new_client(net_to_ui.clone(), id.to_owned(), ip.to_owned()).await;
                 match res {
-                    Ok(_) => println!("Socket Open"),
+                    Ok(sender) => {
+                        connection_map.insert(id.to_owned(), sender);
+                     },
                     Err(err) => println!("{:?}", err),
                 }
             },
@@ -32,12 +42,36 @@ pub async fn network_processor(ui_to_network: &mut Receiver<Message>, network_to
 }
 
 
-pub fn handle_new_client(network_to_ui: Sender<Message>, id: String, ip: String) -> Result<()> {
+pub async fn handle_new_client(
+    network_to_ui: Sender<Message>,
+    id: String,
+    ip: String,
+) -> Result<SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, tungstenite::Message>, anyhow::Error> {
+
     let net_to_ui = network_to_ui;
     let url = url::Url::parse(&ip)?;
+
+    let (ws_stream, _ ) = connect_async(url).await?;
+
+    let (write, mut read) = ws_stream.split();
+
+    let idt = id.clone();
     tokio::spawn(async move {
-        let _ = net_to_ui.send(Message::Message { id,  payload: "Hi".to_string() }).await;
-        tokio::time::sleep(Duration::from_secs(5)).await;
+
+        while let Some(result) = read.next().await {
+            match result {
+                Ok(message) => {
+                    let data = message.into_data();
+                    let message_string = String::from_utf8(data).unwrap();
+                    println!("{}", &message_string);
+                    let _ = net_to_ui.send(Message::Message { id: idt.to_owned(),  payload: message_string }).await;
+                }
+                Err(e) => {
+                    eprintln!("Error reading message: {}", e);
+                    break;
+                }
+            }
+        }
     });
-    Ok(())
+    Ok(write)
 }
